c6c6be8b17813605bdff3a410ead28db4f769d1a
Andrew Dunham <andrew@du.nham.ca>
2021-07-24 23:46:48 -0400

    Abstract out some of the publish logic; publish more

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/StackExchange/wmi v1.2.0 // indirect
 	github.com/eclipse/paho.golang v0.9.1-0.20210720121143-21ea80628f17
 	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/oklog/run v1.1.0
 	github.com/shirou/gopsutil/v3 v3.21.6
 	github.com/spf13/pflag v1.0.5
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
--- main.go
+++ main.go
@@ -4,17 +4,22 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
 
 	"github.com/eclipse/paho.golang/autopaho"
 	"github.com/eclipse/paho.golang/paho"
+	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/load"
+	"github.com/shirou/gopsutil/v3/mem"
 	flag "github.com/spf13/pflag"
 )
 
@@ -26,8 +31,8 @@ func main() {
 		connectRetryDelay = flag.Duration("connect-retry-delay", 10*time.Second, "time between connection attempts")
 		clientID          = flag.String("client-id", "computer2mqtt", "client ID to use when connecting to server")
 
-		prefix   = flag.String("prefix", "", "MQTT topic prefix")
-		qos      = flag.Uint8("qos", 0, "QoS byte")
+		prefix = flag.String("prefix", "computer2mqtt/", "MQTT topic prefix")
+		//qos      = flag.Uint8("qos", 0, "QoS byte")
 		interval = flag.Duration("interval", 5*time.Second, "reporting interval")
 
 		debug         = flag.Bool("debug", false, "enable debug")
@@ -58,7 +63,7 @@ func main() {
 			log.Println("mqtt connection up")
 		},
 		OnConnectError: func(err error) {
-			log.Printf("error whilst attempting connection: %s\n", err)
+			log.Printf("error while attempting connection: %s\n", err)
 		},
 		Debug: paho.NOOPLogger{},
 		ClientConfig: paho.ClientConfig{
@@ -91,56 +96,128 @@ func main() {
 	}
 
 	var wg sync.WaitGroup
-
-	// Start off a goRoutine that publishes messages
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			// AwaitConnection will return immediately if connection is up; adding this call stops publication whilst
-			// connection is unavailable.
-			err = cm.AwaitConnection(ctx)
-			if err != nil { // Should only happen when context is cancelled
-				log.Printf("publisher done (AwaitConnection: %s)\n", err)
-				return
+	load := &periodicPublisher{
+		interval:      *interval,
+		bufferSize:    10,
+		wg:            &wg,
+		cm:            cm,
+		topic:         *prefix + "load",
+		printMessages: *printMessages,
+		cb: func(ctx context.Context) ([]byte, error) {
+			loadAvg, err := load.AvgWithContext(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("error fetching load: %w", err)
 			}
 
-			loadAvg, err := load.Avg()
-			if err == nil {
-				msg, err := json.Marshal(map[string]interface{}{
-					"load1":  loadAvg.Load1,
-					"load5":  loadAvg.Load5,
-					"load15": loadAvg.Load15,
-				})
-				if err != nil {
-					panic(err)
+			msg, err := json.Marshal(map[string]interface{}{
+				"load1":  loadAvg.Load1,
+				"load5":  loadAvg.Load5,
+				"load15": loadAvg.Load15,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("error marshaling JSON: %w", err)
+			}
+
+			return msg, nil
+		},
+	}
+	cpu := &periodicPublisher{
+		interval:      *interval,
+		bufferSize:    10,
+		wg:            &wg,
+		cm:            cm,
+		topic:         *prefix + "cpu",
+		printMessages: *printMessages,
+		cb: func(ctx context.Context) ([]byte, error) {
+			numPhys, err := cpu.Counts(false)
+			if err != nil {
+				return nil, fmt.Errorf("error fetching CPU info: %w", err)
+			}
+			numLogical, err := cpu.Counts(true)
+			if err != nil {
+				return nil, fmt.Errorf("error fetching CPU info: %w", err)
+			}
+
+			msg, err := json.Marshal(map[string]interface{}{
+				"num_physical": numPhys,
+				"num_logical":  numLogical,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("error marshaling JSON: %w", err)
+			}
+
+			return msg, nil
+		},
+	}
+	mem := &periodicPublisher{
+		interval:      *interval,
+		bufferSize:    10,
+		wg:            &wg,
+		cm:            cm,
+		topic:         *prefix + "mem",
+		printMessages: *printMessages,
+		cb: func(ctx context.Context) ([]byte, error) {
+			memInfo, err := mem.VirtualMemoryWithContext(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("error fetching memory info: %w", err)
+			}
+
+			msg, err := json.Marshal(map[string]interface{}{
+				"total":        memInfo.Total,
+				"available":    memInfo.Available,
+				"used":         memInfo.Used,
+				"used_percent": memInfo.UsedPercent,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("error marshaling JSON: %w", err)
+			}
+
+			return msg, nil
+		},
+	}
+	times := &periodicPublisher{
+		interval:      *interval,
+		bufferSize:    10,
+		wg:            &wg,
+		cm:            cm,
+		topic:         *prefix + "times",
+		printMessages: *printMessages,
+		cb: func(ctx context.Context) ([]byte, error) {
+			// TODO: not linux-specific
+			stat, err := ioutil.ReadFile("/proc/stat")
+			if err != nil {
+				return nil, fmt.Errorf("error reading /proc/stat: %w", err)
+			}
+
+			msg := make(map[string]interface{})
+
+			lines := strings.Split(string(stat), "\n")
+			for _, line := range lines {
+				fields := strings.Fields(line)
+				if len(fields) == 0 {
+					continue
 				}
 
-				// Publish will block so we run it in a goRoutine
-				go func(msg []byte) {
-					pr, err := cm.Publish(ctx, &paho.Publish{
-						QoS:     *qos,
-						Topic:   *prefix + "load",
-						Payload: msg,
-					})
-					if err != nil {
-						log.Printf("error publishing: %s\n", err)
-					} else if pr != nil && pr.ReasonCode != 0 && pr.ReasonCode != 16 { // 16 = Server received message but there are no subscribers
-						log.Printf("reason code %d received\n", pr.ReasonCode)
-					} else if *printMessages {
-						fmt.Printf("%s\n", msg)
+				switch fields[0] {
+				case "btime":
+					if val, err := strconv.Atoi(fields[1]); err == nil {
+						msg["boot_time"] = val
 					}
-				}(msg)
+				}
 			}
 
-			select {
-			case <-time.After(*interval):
-			case <-ctx.Done():
-				log.Println("publisher done")
-				return
+			msgb, err := json.Marshal(msg)
+			if err != nil {
+				return nil, fmt.Errorf("error marshaling JSON: %w", err)
 			}
-		}
-	}()
+			return msgb, nil
+		},
+	}
+
+	cpu.Run(ctx)
+	load.Run(ctx)
+	mem.Run(ctx)
+	times.Run(ctx)
 
 	// Wait for a signal before exiting
 	sig := make(chan os.Signal, 1)
@@ -148,11 +225,79 @@ func main() {
 	signal.Notify(sig, syscall.SIGTERM)
 
 	<-sig
-	log.Println("signal caught - exiting")
+	log.Println("signal caught")
 	cancel()
 
 	wg.Wait()
 	log.Println("shutdown complete")
+}
+
+type periodicPublisher struct {
+	cb            func(context.Context) ([]byte, error)
+	cm            *autopaho.ConnectionManager
+	topic         string
+	interval      time.Duration
+	bufferSize    int
+	printMessages bool
+	wg            *sync.WaitGroup
+}
+
+func (p *periodicPublisher) Run(ctx context.Context) {
+	msgs := make(chan []byte, p.bufferSize)
+
+	p.wg.Add(2)
+	go p.sendLoop(ctx, msgs)
+	go p.publishLoop(ctx, msgs)
+}
+
+func (p *periodicPublisher) sendLoop(ctx context.Context, publish chan []byte) {
+	defer p.wg.Done()
+	for msg := range publish {
+		msg := msg // capture loop variable
+
+		// AwaitConnection will return immediately if connection is up;
+		// adding this call stops publication while the connection is
+		// unavailable.
+		err := p.cm.AwaitConnection(ctx)
+		if err != nil {
+			return
+		}
+
+		pr, err := p.cm.Publish(ctx, &paho.Publish{
+			Topic:   p.topic,
+			Payload: msg,
+		})
+		if err != nil {
+			log.Printf("error publishing: %s\n", err)
+		} else if pr != nil && pr.ReasonCode != 0 && pr.ReasonCode != 16 { // 16 = Server received message but there are no subscribers
+			log.Printf("reason code %d received\n", pr.ReasonCode)
+		} else if p.printMessages {
+			fmt.Printf("%s\n", msg)
+		}
+	}
+}
+
+func (p *periodicPublisher) publishLoop(ctx context.Context, publish chan []byte) {
+	defer p.wg.Done()
+	defer close(publish)
+	defer log.Printf("publisher %q done", p.topic)
+
+	ticker := time.NewTicker(p.interval)
+	defer ticker.Stop()
+
+	for {
+		msg, err := p.cb(ctx)
+		if err == nil {
+			publish <- msg
+		}
+
+		select {
+		case <-ticker.C:
+		case <-ctx.Done():
+			return
+		}
+	}
+
 }
 
 // logger implements the paho.Logger interface
